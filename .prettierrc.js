855f028029696b66f9e583797e9d4db4af4127d3
Michael Khryashchev <me@xiberfix.dev>
2023-03-15 01:04:48 +0400

    Add Prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    printWidth: 120,
+    semi: false,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    trailingComma: 'all',
+    bracketSpacing: false,
+    arrowParens: 'avoid',
+}

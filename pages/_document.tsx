e6c4d35872e4936a25d81285360ede7a780a94ed
Michael Khryashchev <me@xiberfix.dev>
2023-03-17 19:25:55 +0400

    Add Tailwind Debug Screens

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
                 <meta charSet='utf-8' />
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
             </Head>
-            <body>
+            <body className='debug-screens'>
                 <Main />
                 <NextScript />
             </body>

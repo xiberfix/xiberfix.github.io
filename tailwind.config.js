4ef938be3b5960b2e9a01348ccf7109c218d40f9
Michael Khryashchev <me@xiberfix.dev>
2023-03-18 08:45:57 +0400

    Update markdown style

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,30 @@ module.exports = {
         extend: {
             typography: theme => ({
                 DEFAULT: {
-                    css: {},
+                    css: {
+                        a: {
+                            color: 'inherit',
+                            fontWeight: 'inherit',
+                            textDecoration: 'none',
+                            borderBottom: `2px solid ${theme('colors.blue.300')}`,
+                            '&:hover': {
+                                borderBottom: `2px solid ${theme('colors.blue.500')}`,
+                            },
+                        },
+                        code: {
+                            fontWeight: 'inherit',
+                            backgroundColor: theme('colors.gray.200'),
+                            padding: '0.125rem 0.25rem',
+                            borderRadius: '0.25rem',
+                        },
+                        img: {
+                            borderRadius: '0.375rem',
+                        },
+                        'code::before': null,
+                        'code::after': null,
+                        'blockquote p:first-of-type::before': null,
+                        'blockquote p:last-of-type::after': null,
+                    },
                 },
             }),
         },

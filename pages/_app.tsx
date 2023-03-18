fb1ba2629acceeea28588695a54aa7c5af6be447
Michael Khryashchev <me@xiberfix.dev>
2023-03-19 00:16:23 +0400

    Move <head> tags to _app from _document and other pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import '../styles/global.css'
 import type {AppProps} from 'next/app'
+import Head from 'next/head'
 
 export default function App({Component, pageProps}: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <meta name='author' content='Michael Khryashchev' />
+                <meta name='description' content='Personal Site' />
+
+                <title>Michael Khryashchev</title>
+
+                <meta property='og:title' content='Michael Khryashchev' />
+                <meta property='og:description' content='Personal Site' />
+                <meta property='og:type' content='website' />
+                <meta property='og:url' content='https://xiberfix.dev' />
+                <meta property='og:image' content='' />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }

7e06b1ba9c8d4aea640cda959987e88529b86631
Michael Khryashchev <me@xiberfix.dev>
2023-03-18 12:27:41 +0400

    Add math with KaTeX

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,15 @@ import nextMDX from '@next/mdx'
 import remarkGfm from 'remark-gfm'
 import remarkFrontmatter from 'remark-frontmatter'
 import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
 
 // MDX plugin
 const mdxConfig = {
     extension: /\.mdx?$/,
     options: {
-        remarkPlugins: [remarkGfm, remarkFrontmatter, remarkMdxFrontmatter],
-        rehypePlugins: [],
+        remarkPlugins: [remarkGfm, remarkFrontmatter, remarkMdxFrontmatter, remarkMath],
+        rehypePlugins: [rehypeKatex],
     },
 }
 

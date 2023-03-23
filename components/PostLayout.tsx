6a7eab64f6a5b742c97167b49fcf1b2e999cd5b7
Michael Khryashchev <me@xiberfix.dev>
2023-03-24 00:13:31 +0400

    Add title to post pages

--- components/PostLayout.tsx
+++ components/PostLayout.tsx
@@ -1,9 +1,10 @@
+import Head from 'next/head'
 import {PropsWithChildren} from 'react'
 import Layout from '@components/Layout'
 
 interface PostMeta {
-    title?: string
-    date?: string
+    title: string
+    date: string
 }
 
 interface PostLayoutProps {
@@ -11,14 +12,19 @@ interface PostLayoutProps {
 }
 
 export default function PostLayout({meta, children}: PropsWithChildren<PostLayoutProps>) {
-    const {title, date} = meta
+    const {title = '', date = ''} = meta
 
     return (
-        <Layout>
-            {title && <h1 className='text-4xl font-bold text-slate-800 sm:text-5xl'>{title}</h1>}
-            <article className='my-10 prose prose-sm sm:prose-base sm:my-16'>{children}</article>
-            {date && <time className='text-sm text-slate-400 sm:text-base'>{DATE_FORMAT.format(new Date(date))}</time>}
-        </Layout>
+        <>
+            <Head>
+                <title>{title} | xiberfix.dev</title>
+            </Head>
+            <Layout>
+                <h1 className='text-4xl font-bold text-slate-800 sm:text-5xl'>{title}</h1>
+                <article className='my-10 prose prose-sm sm:prose-base sm:my-16'>{children}</article>
+                <time className='text-sm text-slate-400 sm:text-base'>{DATE_FORMAT.format(new Date(date))}</time>
+            </Layout>
+        </>
     )
 }
 
